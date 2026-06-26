46cb338521518dac5324d52210c7d08f3e2284fe
sararwil <srjw28@gmail.com>
2026-06-26 09:32:16 -0400

    updated to work for any nth degree polynomial'
    '

--- ols.cpp
+++ ols.cpp
@@ -7,95 +7,137 @@
 using namespace std;
 
 int main() {
-  // set up system
-vector<double> x = {1, 3, 4, 6, 7, 8, 10, 11, 13, 14, 15, 17, 18, 20, 21, 22, 24, 25, 27, 28};
-vector<double> y = {3.2, 7.1, 6.4, 9.8, 8.3, 12.5, 11.2, 14.7, 13.1, 17.4, 15.9, 18.2, 20.6, 19.3, 22.8, 21.4, 24.1, 26.7, 25.3, 28.9};
 
-int n = x.size();
+// polynomial degree
+int n = 3;
 
-    vector<vector<double>> A(n, vector<double>(2, 0.0));
+vector<double> x = {-3, -2, -1, 0, 1, 2, 3, 4, 5, 6};
 
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < 2; j++){
+// y = 1 + 2x + 3x^2 + 4x^3
+vector<double> y = {
+    4*(-27) + 3*9  + 2*(-3) + 1,   // -98
+    4*(-8)  + 3*4  + 2*(-2) + 1,   // -23
+    4*(-1)  + 3*1  + 2*(-1) + 1,   //   0
+    4*(0)   + 3*0  + 2*(0)  + 1,   //   1
+    4*(1)   + 3*1  + 2*(1)  + 1,   //  10
+    4*(8)   + 3*4  + 2*(2)  + 1,   //  41
+    4*(27)  + 3*9  + 2*(3)  + 1,   // 112
+    4*(64)  + 3*16 + 2*(4)  + 1,   // 233
+    4*(125) + 3*25 + 2*(5)  + 1,   // 416
+    4*(216) + 3*36 + 2*(6)  + 1    // 673
+};
+
+
+int m = x.size();
+// A
+    vector<vector<double>> A(m, vector<double>(n+1, 0.0));
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < (n+1); j++){
                 A[i][j] = pow(x[i], j);
             }
         }
+    // A transpose
+    vector<vector<double>> A_T((n+1), vector<double>(m,0.0));
     
-    vector<vector<double>> A_T(2, vector<double>(n,0.0));
-    
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < 2; j++){
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < (n+1); j++){
             A_T[j][i] = A[i][j];
         }
     }
+    // matrix multiplication
+    vector<vector<double>> matrix_mult((n+1), vector<double>((n+1),0.0));
 
-    vector<vector<double>> matrix_mult(2, vector<double>(2,0.0));
-
-    for (int i = 0; i < 2; i++){
-        for (int j = 0; j < 2; j ++){
-            for (int k = 0; k < n; k++){
+    for (int i = 0; i < (n+1); i++){
+        for (int j = 0; j < (n+1); j ++){
+            for (int k = 0; k < m; k++){
                 matrix_mult[i][j] += A_T[i][k] * A[k][j];
              }
         }
     }
+    // determinant
 
-    double a = matrix_mult[0][0];
-    double b = matrix_mult[0][1];
-    double c = matrix_mult[1][0];
-    double d = matrix_mult[1][1];
+    vector<double> A_T_b((n+1), 0.0);
 
-    double det = (a*d) - (b*c);
-
-    vector<vector<double>> matrix_mult_inv(2, vector<double>(2,0.0));
-
-    if (abs(det) < numeric_limits<double>::epsilon()){
-        cout << "Matrix cannot be inverted. \n";
-    }
-    else {
-        double inv_det = 1.0 / det;
-        matrix_mult_inv[0][0] = d*inv_det;
-        matrix_mult_inv[0][1] = -b*inv_det;
-        matrix_mult_inv[1][0] = -c*inv_det;
-        matrix_mult_inv[1][1] = a*inv_det;
-    }
-
-    vector<double> A_T_b(2,0.0);
-    
-    for (int i = 0; i < 2; i++){
-        for (int j = 0; j < n; j++){
+    // A transpose times b
+    for ( int i = 0; i < (n+1); i++){
+        for( int j = 0; j < m; j++) {
             A_T_b[i] += A_T[i][j] * y[j];
         }
     }
 
-    vector<double> ols(2,0.0);
+    // calculate inverse
 
-    for(int i = 0; i < 2; i ++){
-        for (int j = 0; j < 2; j++){
-            ols[i] += matrix_mult_inv[i][j] * A_T_b[j];
+    // build augmented matrix, then gaussian elim
+    vector<vector<double>> aug( (n+1), vector<double>((n+2), 0.0));
+        for (int i = 0; i < (n+1); i++) {
+            for (int j = 0; j < (n+1); j++) {
+                aug[i][j] = matrix_mult[i][j];
+            }
+            aug[i][n+1] = A_T_b[i];
         }
+
+    // forward elim
+    for ( int j = 0; j < (n+1); j++) {
+        int pivot = -1;
+        double best = 0.0;
+        for ( int i = j; i < (n+1); i++) {
+            if (abs(aug[i][j]) > best) {
+                best = abs(aug[i][j]);
+                pivot = i;
+            } 
+        }
+        if (pivot == -1) {
+            cout << "Matrix is singular and cannot be inverted. \n";
+        }
+        swap(aug[j],aug[pivot]);
+        for (int i = (j+1); i< (n+1); i++) {
+            double mult = aug[i][j] / aug[j][j];
+            for(int k = j; k < (n+2); k++){
+                aug[i][k] -= mult * aug[j][k];
+            }
+        }  
     }
 
+    // back sub
+    vector<double> ols((n+1),0.0);
+    for( int i = n; i >= 0; i--) {
+        ols[i] = aug[i][n+1];
+        for (int j = (i+1); j < (n+1); j++) {
+            ols [ i] -= aug[i][j] * ols[j];
+        }
+        ols[i] /= aug[i][i];
+    }
+
+
+
+    // calculate root mean square error (rmse)
     double rmse = 0.0;
 
-    for (int i = 0; i < n; i++){
-        double y_hat = ols[0] + ols[1] * x[i];
-        rmse += pow(y_hat - y[i], 2);
+    for (int i = 0; i < m; i++){
+        double yhat = 0.0;
+        for (int j = 0; j < (n+1); j++) {
+            yhat += ols[j] * pow(x[i], j);
+        }
+        rmse += pow(yhat - y[i], 2);
     }
 
-    rmse = sqrt(rmse/n);
+    rmse = sqrt(rmse/m);
 
-    cout << "intercept: " << ols[0] << "\n";
-    cout << "slope: " << ols[1] << "\n";
+    for (int i = 0; i < (n+1); i++){
+        cout << " coeff[" << i << "]: " << ols[i] << "\n";
+    }
     cout << "rmse: " << rmse << "\n";
-
+    
+    // prep python plot
     ofstream file("ols_cpp_output.csv");
-    file << "x,y,y_hat\n";
-    for(int i = 0; i < n; i++){
-        double y_hat = 0.0;
-        for(int j = 0; j < 2; j++){
-            y_hat += A[i][j] * ols[j];
+    file << "x,y,yhat\n";
+    for(int i = 0; i < m; i++){
+        double yhat = 0.0;
+        for(int j = 0; j < (n+1); j++){
+            yhat += A[i][j] * ols[j];
         }
-        file << x[i] << "," << y[i] << "," << y_hat << "\n";
+        file << x[i] << "," << y[i] << "," << yhat << "\n";
     }
     file.close();
 
--- ols_cpp_plot.py
+++ ols_cpp_plot.py
@@ -2,9 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("ols_cpp_output.csv")
+df = df.sort_values("x")
 
 plt.scatter(df['x'], df['y'], label='data', color='red')
-plt.plot(df['x'], df['y_hat'], label="C++ OLS", color="green")
+plt.plot(df['x'], df['yhat'], label="C++ OLS", color="green")
 plt.xlabel('x')
 plt.ylabel('y')
 plt.legend()
