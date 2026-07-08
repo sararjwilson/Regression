745e6146759006a2216b2944a23ad81868cd6b81
sararwil <srjw28@gmail.com>
2026-07-08 18:28:43 -0400

    fixed

--- SINDy/sindy_sho.cpp
+++ SINDy/sindy_sho.cpp
@@ -11,89 +11,89 @@ using namespace Eigen;
 
 // set up parameters 
 const double omega = 1.0;
-double tol = numeric_limits<double>::epsilon();
+double tol = numeric_limits<double>::epsilon(); // machine epsilon 
 
 // direct ode computation
-Vector2d ode_rhs(const Vector2d& state) {
+Vector2d ode_rhs(const Vector2d& traj) {
 // system dynamics: simple harmonic oscillator
 // d^2x/dt^2 = - omega_0^2 x
 
-    double x = state(0);
-    double v = state(1);
-    double a = -omega * x;
+    double x = traj(0);
+    double v = traj(1);
+    double a = - pow(omega,2) * x;
 
     return Vector2d(v,a);
 }
 
 // RK4 for second order ode to generate data 
-Vector2d rk4step(const Vector2d& state, double dt) {
+Vector2d rk4step(const Vector2d& traj, double dt) {
 
-    Vector2d k1 = ode_rhs(state);
-    Vector2d k2 = ode_rhs(state + k1 * (dt/2));
-    Vector2d k3 = ode_rhs(state + k2 * (dt/2));
-    Vector2d k4 = ode_rhs(state + k3 * dt);
+    Vector2d k1 = ode_rhs(traj);
+    Vector2d k2 = ode_rhs(traj + k1 * (dt/2));
+    Vector2d k3 = ode_rhs(traj + k2 * (dt/2));
+    Vector2d k4 = ode_rhs(traj + k3 * dt);
 
-    Vector2d state_next = state + (dt/6)*(k1 + 2*k2 + 2*k3 + k4);
-    return state_next;
+    Vector2d traj_next = traj + (dt/6)*(k1 + 2*k2 + 2*k3 + k4);
+    return traj_next;
 }
 
 // central difference for derivative terms in candidate library
-vector<Vector2d> fd(const vector<Vector2d>& states, double dt) {
-    int n = states.size();
+vector<Vector2d> fd(const vector<Vector2d>& trajs, double dt) {
+    int n = trajs.size();
     vector<Vector2d> fd_est;
 
     for (int i = 1; i < (n-1); i++){
-       Vector2d fd_steps = (states[i+1] - states[i-1]) / (2*dt);
+       Vector2d fd_steps = (trajs[i+1] - trajs[i-1]) / (2*dt);
        fd_est.push_back(fd_steps);
     }
     return fd_est;
 }
 
 // noise generation
-vector<Vector2d> add_noise(const vector<Vector2d>& states, double noise_lvl){
+vector<Vector2d> add_noise(const vector<Vector2d>& trajs, double noise_lvl){
     static random_device rd;
     static default_random_engine generator(rd());
     normal_distribution<double> distribution(0.0, noise_lvl);
-    vector<Vector2d> states_noise(states.size());
+    vector<Vector2d> trajs_noise(trajs.size());
 
-    for(int i = 0; i < states.size(); i++){
+    for(int i = 0; i < trajs.size(); i++){
         double x_noise = distribution(generator); // noise gen should just be on x? ask about this
         double v_noise = distribution(generator);
 
-        states_noise[i] = Vector2d(states[i](0) + x_noise, states[i](1) + v_noise);
+        trajs_noise[i] = Vector2d(trajs[i](0) + x_noise, trajs[i](1) + v_noise);
     }
-    return states_noise;
+    return trajs_noise;
 }
 
 // SINDy set-up
-RowVectorXd build_library_row(const Vector2d& state) {
-    double x = state(0);
-    double v = state(1);
+RowVectorXd build_library_row(const Vector2d& traj) {
+    double x = traj(0);
+    double v = traj(1);
 
     RowVectorXd row(6);
     row << 1, x, v, x*x, v*v, x*v;
     return row;
 }
 
-MatrixXd build_library(const vector<Vector2d>& states){
-    int n = states.size();
+MatrixXd build_library(const vector<Vector2d>& trajs){
+    int n = trajs.size();
     int num_terms = 6; 
     
     MatrixXd Theta(n, num_terms);
 
     for(int i = 0; i < n; i++){
-        Theta.row(i) = build_library_row(states[i]);
+        Theta.row(i) = build_library_row(trajs[i]);
     }
     return Theta;
 }
 
 // collection of derivatives evaluated at each time step
-VectorXd ground_truth_derivatives(const vector<Vector2d>& states, int component){
-    int n = states.size();
+VectorXd ground_truth_derivatives(const vector<Vector2d>& trajs, int component){
+    int n = trajs.size();
     VectorXd target(n); 
 
     for (int i = 0; i < n; i++){
-        Vector2d derivative = ode_rhs(states[i]);
+        Vector2d derivative = ode_rhs(trajs[i]);
         target(i) = derivative(component);
     }
     return target;
@@ -137,9 +137,9 @@ VectorXd stlsq(const MatrixXd& Theta, const VectorXd& target, double lambda){
 }
 
 // recover ode
-void print_equation(const VectorXd& xi, const string& state_name) {
+void print_equation(const VectorXd& xi, const string& traj_name) {
     vector<string> term_names = { "1", "x" , "v", "x^2", "v^2", "xv"};
-    cout << "d" << state_name << "/dt = " << "\n";
+    cout << "d" << traj_name << "/dt = " << "\n";
 
     bool first = true;
     for (int i = 0; i < xi.size(); i++){
@@ -163,24 +163,24 @@ int main() {
     int n = static_cast<int>(t_end/dt);
 
     // initial conditions for position and velocity
-    Vector2d state(1,0);
+    Vector2d traj(1,0);
 
-    vector<Vector2d> states;
+    vector<Vector2d> trajs;
     vector<double> times;
 
     double t = 0.0;
     for (int i = 0; i < n; i++){
-        states.push_back(state);
+        trajs.push_back(traj);
         times.push_back(t);
 
-        state = rk4step(state, dt);
+        traj = rk4step(traj, dt);
         t += dt;
     }
 
     double noise_lvl = 0.0;
-    vector<Vector2d> states_noise = add_noise(states, noise_lvl);
+    vector<Vector2d> trajs_noise = add_noise(trajs, noise_lvl);
 
-    vector<Vector2d> fd_est = fd(states_noise, dt);
+    vector<Vector2d> fd_est = fd(trajs_noise, dt);
     int m = fd_est.size();
     VectorXd v_target_noise(m);
     VectorXd a_target_noise(m);
@@ -192,12 +192,12 @@ int main() {
 
 
     // SINDy
-    MatrixXd Theta = build_library(states); // candidate library
-    vector<Vector2d> states_noise_matched(states_noise.begin() + 1, states_noise.end() - 1);
-    MatrixXd Theta_noise = build_library(states_noise_matched);
+    MatrixXd Theta = build_library(trajs); // candidate library
+    vector<Vector2d> trajs_noise_matched(trajs_noise.begin() + 1, trajs_noise.end() - 1);
+    MatrixXd Theta_noise = build_library(trajs_noise_matched);
 
-    VectorXd v_target = ground_truth_derivatives(states,0);
-    VectorXd a_target = ground_truth_derivatives(states,1);
+    VectorXd v_target = ground_truth_derivatives(trajs,0);
+    VectorXd a_target = ground_truth_derivatives(trajs,1);
 
     // stlsq
     VectorXd xi_v_final = stlsq(Theta_noise, v_target_noise, 0.05);
@@ -210,10 +210,10 @@ int main() {
     ofstream outfile("sho_data.csv");
     outfile << "t,x,v\n";
     for (int i = 0; i < n; i++) {
-        outfile << times[i] << "," << states[i](0) << "," << states[i](1) << "\n";
+        outfile << times[i] << "," << trajs[i](0) << "," << trajs[i](1) << "\n";
     }
     outfile.close();
 
-    // python3 sindy_lorenz_plot.py
+    // python3 sindy_sho_plot.py
 
 }
